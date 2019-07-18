51d13258cd36d25f0fe3e80661cede03835c10b6
Sudhanshu Kumar <sudhanshu@peppersquare.com>
2019-07-18 13:06:29 +0530

    fetched and displayed from api

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -1,26 +1,23 @@
 import React from "react";
 import "./card.css";
 
-const Card = props => {
+const Card = ({ post }) => {
+  console.log(new Date(post.created_at))
+  const date = new Date(post.created_at).getDate()
+  const month = new Date(post.created_at).toLocaleString('default', { month: 'short' })
+  const year = new Date(post.created_at).getFullYear()
   return (
     <div className="card">
-      <img
-        className="card-img-top"
-        src="https://researchandbibliographywebquest.files.wordpress.com/2014/08/book-shelf.jpg"
-        alt="Card image cap"
-      />
+      <img className="card-img-top" src={post.image} alt="Card image cap" />
       <div className="card-body">
-        <h3 className="card-title">Quick Brown Fox Jump Over</h3>
+        <h3 className="card-title">{post.title}</h3>
         <div className="card-sub-title">
-          <h5>TRAVELLING</h5>
-          <h5>16 AUG 2016</h5>
+          <h5>{post.tags[0]}</h5>
+          <h5>{date} {month} {year}</h5>
         </div>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
+        <p className="card-text">{post.description}</p>
         <hr />
-        <h5 className="likes">Likes 150</h5>
+        <h5 className="likes">Likes {post.likes}</h5>
       </div>
     </div>
   );
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,26 +1,29 @@
 import React, { Component } from "react";
-//import axios from "axios";
+import axios from "axios";
 import Card from "../cards/card";
 
 class Home extends Component {
-  state = {};
+  state = {
+    posts: []
+  };
 
-  //   componentDidMount() {
-  //     axios
-  //       .get("http://test.peppersquare.com/api/v1/article")
-  //       .then(res => res.json())
-  //       .then(data => {
-  //         console.log(data);
-  //       })
-  //       .catch(err => console.log(err));
-  //   }
+    componentDidMount() {
+      axios
+        .get("http://test.peppersquare.com/api/v1/article")
+        .then((res) => {
+          console.log(res.data);
+          this.setState({ posts: res.data })
+        })
+        .catch(err => console.log(err));
+    }
 
   render() {
+    const { posts } = this.state
     return (
       <div>
-        <Card />
-        <Card />
-        <Card />
+        {posts.map((post, index) => {
+          return <Card key={index} post={post}></Card>
+        })}
       </div>
     );
   }
