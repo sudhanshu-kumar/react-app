6ce8cb00060fc3b47d96ef5ef01ef7bd3c13054d
Sudhanshu Kumar <sudhanshu@peppersquare.com>
2019-07-25 10:50:32 +0530

    show newly added blog at top

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,14 +4,15 @@ import Card from "../cards/card";
 
 //const Card = lazy(() => import("../cards/card"));
 
-const Home = props => {
+const Home = ({posts}) => {
+  const newPosts = [...posts].sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime() )
   return (
     <div>
       <div className="header">
         <h2>Home</h2>
       </div>
       <div className="posts">
-        {props.posts.map((post, index) => {
+        {newPosts.map((post, index) => {
           return (
             // <Suspense fallback={<div>Loading...</div>}>
               <Card key={index} post={post} />
