eaa4a5c8663816f5d35e1b62d4a29e3d7523c05c
Sudhanshu Kumar <sudhanshu@peppersquare.com>
2019-08-05 16:50:19 +0530

    search on click of list items and using arrow key

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -10,28 +10,87 @@ import { getTitles } from "../../helpers/blogTitles";
 
 class Home extends Component {
   state = {
-    search: ""
+    search: "",
+    listOfFilteredTitles: [],
+    newPosts: [],
+    activeTitle: 0
   };
 
+  componentDidMount() {
+    this.setState({ newPosts: sortByNewAdded(this.props.posts) });
+  }
+
   handleSearch = event => {
-    console.log(event.target.value);
-    this.setState({ search: event.target.value });
+    console.log(event.target);
+    let searchString = event.target.value;
+    let listOfFilteredTitles = [];
+    let newPosts = sortByNewAdded(this.props.posts);
+    if (searchString.length > 0) {
+      let filteredBlogs = searchBlogs(this.props.posts, searchString);
+      listOfFilteredTitles =
+        filteredBlogs.length < newPosts.length ? getTitles(filteredBlogs) : [];
+      newPosts = sortByNewAdded(filteredBlogs);
+    }
+    this.setState({
+      search: event.target.value,
+      listOfFilteredTitles,
+      newPosts
+    });
+  };
+
+  handleKeyDown = event => {
+    const { activeTitle, listOfFilteredTitles } = this.state;
+    console.log(event.keyCode);
+    let newList = [];
+    if (event.keyCode === 13) {
+      let newPosts = sortByNewAdded(
+        searchBlogs(this.props.posts, listOfFilteredTitles[activeTitle])
+      );
+      if (newPosts.length > 1) {
+        newList = getTitles(newPosts);
+      }
+      this.setState({
+        activeTitle: 0,
+        search: listOfFilteredTitles[activeTitle],
+        listOfFilteredTitles: newList,
+        newPosts
+      });
+    }
+    else if (event.keyCode === 40) {
+      if (activeTitle + 1 === listOfFilteredTitles.length) {
+        return;
+      }
+      this.setState({ activeTitle: activeTitle + 1 });
+    }
+    else if (event.keyCode === 38) {
+      if (activeTitle === 0) {
+        return;
+      }
+      this.setState({ activeTitle: activeTitle - 1 });
+    }
   };
 
   onClickListItem = event => {
-    console.log(event.target.innerHTML);
-    this.setState({ search: event.target.innerHTML });
+    console.log(event.target);
+    let listOfFilteredTitles = [];
+    let newPosts = sortByNewAdded(
+      searchBlogs(this.props.posts, event.target.innerText)
+    );
+    if (newPosts.length > 1) {
+      listOfFilteredTitles = getTitles(newPosts);
+    }
+    this.setState({
+      search: event.target.innerText,
+      listOfFilteredTitles,
+      newPosts
+    });
   };
 
   render() {
-    let filteredBlogs = searchBlogs(this.props.posts, this.state.search);
-    let listOfFilteredTitles =
-      getTitles(filteredBlogs).length < this.props.posts.length
-        ? getTitles(filteredBlogs)
-        : [];
-    let newPosts = sortByNewAdded(filteredBlogs);
-    let listClass = listOfFilteredTitles.length !== 0 ? "title-list" : "no-title-list"
-    console.log("titles", listOfFilteredTitles);
+    const { listOfFilteredTitles, newPosts, activeTitle } = this.state;
+    let listClass =
+      listOfFilteredTitles.length !== 0 ? "title-list" : "no-title-list";
+    console.log("titles", activeTitle);
     return (
       <div>
         <div className="header">
@@ -44,13 +103,23 @@ class Home extends Component {
               placeholder="Search..."
               value={this.state.search}
               onChange={this.handleSearch}
-              onBlur={(e) => console.log(e)}
+              onKeyDown={this.handleKeyDown}
+              // onBlur={e => (e.target.nextSibling.style.display = "none")}
+              // onFocus={e => (e.target.nextSibling.style.display = "inline")}
             />
-            <div className={listClass}>
+            <div className={listClass} onBlur={e => console.log(e)}>
               {listOfFilteredTitles.length !== 0
                 ? listOfFilteredTitles.map((title, index) => {
+                    let className = "list-item";
+                    if (index === activeTitle) {
+                      className = "list-item active-title";
+                    }
                     return (
-                      <div key={index} onClick={this.onClickListItem} className="list-item">
+                      <div
+                        key={index}
+                        onClick={this.onClickListItem}
+                        className={className}
+                      >
                         {title}
                       </div>
                     );
--- src/helpers/searchBlogs.js
+++ src/helpers/searchBlogs.js
@@ -1,6 +1,7 @@
 import { isArray } from "util";
 
 export const searchBlogs = (blogs, searchText) => {
+    console.log(searchText)
     console.log(typeof(blogs[0].title))
     if(isArray(blogs) && blogs.length > 0) return blogs.filter((blog) => blog.title.toLowerCase().indexOf(searchText.toLowerCase()) !== -1)
 }
